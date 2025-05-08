db2725c31b268b954790d37971d9adaa48b56940
fruitshub <msmoshiurmr@gmail.com>
2025-05-09 00:54:45 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 document.getElementById('langToggle').addEventListener('click', function() {
   alert('Language switching will be added soon.');
 });
+
+document.querySelectorAll('.lang-toggle').forEach(btn => {
+  btn.addEventListener('click', () => {
+    const lang = btn.dataset.lang;
+    document.querySelectorAll('.lang').forEach(el => {
+      el.innerText = el.dataset[lang];
+    });
+  });
+});
